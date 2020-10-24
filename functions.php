1ab7b7dee9f98da7e909ca56d9762414655b857a
Barry Mackie-Conlon <hello@barryconlon.com>
2020-10-24 16:08:17 +0100

    added basic navigation and removed potentially unused default widgets

--- functions.php
+++ functions.php
@@ -16,10 +16,43 @@ define( 'VULPIX_ROOT_URI', get_template_directory_uri() );
 
 /**
  * Theme Setup
+ *
+ * @since    1.0.0
  */
 function vulpix_theme_setup() {
 
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'html5', [ 'style', 'script' ] );
+
+	register_nav_menus(
+		array(
+			'main_menu'      => 'Main Menu',
+			'footer_menu'    => 'Footer Menu',
+			'shop_menu'      => 'Shop Menu',
+		)
+	);
 }
 add_action( 'after_setup_theme', 'vulpix_theme_setup' );
+
+
+/**
+ * Remove default widgets from WP.
+ *
+ * @since    1.0.0
+ */
+function vulpix_remove_default_widgets() {
+	unregister_widget( 'WP_Widget_Pages' );
+	unregister_widget( 'WP_Widget_Calendar' );
+	unregister_widget( 'WP_Widget_Archives' );
+	unregister_widget( 'WP_Widget_Links' );
+	unregister_widget( 'WP_Widget_Meta' );
+	unregister_widget( 'WP_Widget_Recent_Comments' );
+	unregister_widget( 'WP_Widget_RSS' );
+	unregister_widget( 'WP_Widget_Tag_Cloud' );
+	unregister_widget( 'WP_Nav_Menu_Widget' );
+	unregister_widget( 'WP_Widget_Media_Video' );
+	unregister_widget( 'WP_Widget_Media_Audio' );
+	unregister_widget( 'WP_Widget_Media_Gallery' );
+	unregister_widget( 'WP_Widget_Custom_HTML' );
+}
+add_action( 'widgets_init', 'vulpix_remove_default_widgets', 11 );

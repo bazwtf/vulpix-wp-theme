a66e15bbd431dfe5340b249c9cafd867ea65816e
Barry Mackie-Conlon <hello@baz.wtf>
2020-11-29 18:14:08 +0000

    post list template updated

--- template-parts/template-post-list.php
+++ template-parts/template-post-list.php
@@ -7,11 +7,15 @@ defined( 'ABSPATH' ) or die( 'Vulpix, use Flamethrower!' );
  * @since Vulpix 1.0.0
  */
 ?>
-<article class="col-12">
-    <?php
-    vpx_the_post_thumbnail();
-    the_category();
-    the_title();
-    vpx_the_post_meta();
-    ?>
+<article class="post-list container-full">
+    <div class="col-6 post-list__thumbnail-container">
+        <?php vpx_the_post_thumbnail(); ?>
+    </div>
+    <div class="col-6 post-list__details">
+        <?php
+        the_category();
+        the_title();
+        vpx_the_post_meta();
+        ?>
+    </div>
 </article>

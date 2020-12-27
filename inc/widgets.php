3f68cd22415a49c2e641017e2f2c1bd974d30747
Barry Mackie-Conlon <hello@baz.wtf>
2020-12-27 02:13:12 +0000

    more updates to panel widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -7,40 +7,59 @@ defined( 'ABSPATH' ) or die( 'Vulpix, use Flamethrower!' );
  * @since Vulpix 1.0.0
  */
 
+/**
+ * Class vpx_panel_widget
+ */
 class vpx_panel_widget extends WP_Widget {
 
+    /**
+     * vpx_panel_widget constructor
+     *
+     * @since 1.0.0
+     * @return void
+     */
     function __construct() {
         parent::__construct(
-
-            // Base ID of widget
             'vpx_panel_widget',
-
-            // Widget name
             __( 'Info panel', 'vulpix' ),
-
-            // Widget description
             [
                 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'vulpix' ),
             ]
         );
     }
 
-    // Creating widget front-end
+    /**
+     * Widget front-end
+     *
+     * @since 1.0.0
+     * @param array $args
+     * @param array $instance
+     * @return void
+     */
     public function widget( $args, $instance ) {
         $title = apply_filters( 'widget_title', $instance['title'] );
 
-        // before and after widget arguments are defined by themes
         echo $args['before_widget'];
         if ( ! empty( $title ) ) {
             echo $args['before_title'] . $title . $args['after_title'];
         }
 
-        // This is where you run the code and display the output
+        // Panel Text
+
+        // Panel Button
+        // TODO: Content for widget ouput
         echo __( 'Hello, World!', 'vulpix' );
+
         echo $args['after_widget'];
     }
 
-    // Widget Backend
+    /**
+     * Widget backend
+     *
+     * @since 1.0.0
+     * @param array $instance
+     * @return string|void
+     */
     public function form( $instance ) {
         if ( isset( $instance[ 'title' ] ) ) {
             $title = $instance[ 'title' ];
@@ -48,16 +67,36 @@ class vpx_panel_widget extends WP_Widget {
         else {
             $title = __( 'New title', 'vulpix' );
         }
-        // Widget admin form
-        ?>
-        <p>
-            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-        </p>
-        <?php
+
+        /**
+         * Widget admin form
+         */
+
+        // Panel Title
+        printf(
+            '<p>
+                <label for="%1$s">%2$s</label>
+                <input class="widefat" id="%1$s" name="%3$s" type="text" value="%4$s" />
+            </p>',
+            $this->get_field_id( 'title' ),
+            __( 'Panel Title: ', 'vulpix' ),
+            $this->get_field_name( 'title' ),
+            esc_attr( $title )
+        );
+
+        // Panel Text
+
+        // Panel Button
     }
 
-    // Updating widget replacing old instances with new
+    /**
+     * Update widget replacing old instances with new
+     *
+     * @since 1.0.0
+     * @param array $new_instance
+     * @param array $old_instance
+     * @return array
+     */
     public function update( $new_instance, $old_instance ) {
         $instance = [];
         $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
@@ -65,8 +104,12 @@ class vpx_panel_widget extends WP_Widget {
     }
 }
 
-
-// Register and load the widget
+/**
+ * Register and load the widget
+ *
+ * @since 1.0.0
+ * @return void
+ */
 function vpx_load_widget() {
     register_widget( 'vpx_panel_widget' );
 }

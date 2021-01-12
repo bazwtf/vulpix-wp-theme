93ba880254592f505ba03a558e503578d6646925
Barry Mackie-Conlon <hello@baz.wtf>
2021-01-12 00:34:47 +0000

    wrapper added to info panels to fix layout

--- template-parts/template-panels.php
+++ template-parts/template-panels.php
@@ -7,6 +7,10 @@ defined( 'ABSPATH' ) or die( 'Vulpix, use Flamethrower!' );
  * @since Vulpix 1.0.0
  */
 
+echo '<div class="grid">';
+
 vpx_sidebar( 'sidebar-footer-left', 'col-sm-4' );
 vpx_sidebar( 'sidebar-footer-center', 'col-sm-4' );
 vpx_sidebar( 'sidebar-footer-right', 'col-sm-4' );
+
+echo '</div>';

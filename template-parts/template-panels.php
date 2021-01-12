e1e34a38c134897892033d96918b906b38ecee2a
Barry Mackie-Conlon <hello@baz.wtf>
2021-01-12 00:57:29 +0000

    comments added to info panel functions in template-panels.php

--- template-parts/template-panels.php
+++ template-parts/template-panels.php
@@ -9,8 +9,13 @@ defined( 'ABSPATH' ) or die( 'Vulpix, use Flamethrower!' );
 
 echo '<div class="grid">';
 
+// Get `sidebar-footer-left`
 vpx_sidebar( 'sidebar-footer-left', 'col-sm-4' );
+
+// Get `sidebar-footer-center`
 vpx_sidebar( 'sidebar-footer-center', 'col-sm-4' );
+
+// Get `sidebar-footer-right`
 vpx_sidebar( 'sidebar-footer-right', 'col-sm-4' );
 
 echo '</div>';
